c117b992d09d7f19b32c4a6797fae67135b92778
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-04 00:28:45 +0000

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: no parameter
+ *
+ * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * Return:  0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
