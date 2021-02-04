109aa90b26628888379db1547d8387faa59c8cb8
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-04 23:37:19 +0000

    Add 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,22 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  * @void: no parameter
  *
- * Description: Prints message using prinf function
+ * Description: Prints message using putchar
  * Return:  0
  */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+}
 
 
