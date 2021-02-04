5807289c50ed0369adf1686a31a2252748623b51
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-04 00:04:04 +0000

    Added 0x00-hello_world 0-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: no parameter
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle
+ * Return:  0
+ */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
