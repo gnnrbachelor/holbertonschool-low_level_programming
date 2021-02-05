08c3dfe455fbe365a5a6e91ca204601f6592fa82
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-05 16:01:32 +0000

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: no parameter
+ *
+ * Description: Prints message using prinf function
+ * Return:  0
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
+	return (1);
+}
