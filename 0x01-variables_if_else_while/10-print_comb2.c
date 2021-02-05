f727f0710ad29215461760af0bfb2c3972905307
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-05 02:07:56 +0000

    Add 10-print_comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: no parameter
+ *
+ * Description: Prints # 00 to 99
+ * Return:  0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
