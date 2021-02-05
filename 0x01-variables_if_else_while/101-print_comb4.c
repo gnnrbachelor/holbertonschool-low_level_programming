8ad2f672d1a0c1f581e97ea74c71ff22af4ac52a
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-05 17:52:53 +0000

    Fixed 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,7 +24,7 @@ int main(void)
 				putchar((hundreds % 10) + '0');
 				putchar((tens % 10) + '0');
 				putchar((ones % 10) + '0');
-				if (tens == 8 && ones == 9)
+				if (hundreds == 7 && tens == 8 && ones == 9)
 					break;
 				putchar(',');
 				putchar(' ');
