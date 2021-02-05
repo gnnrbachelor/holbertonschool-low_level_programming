c69d86cdc9977d5d69ef537bdf569c9056d567df
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-05 16:05:30 +0000

    Fix 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  * @void: no parameter
  *
- * Description: Prints message using prinf function
+ * Description: Prints message without using prinf function
  * Return:  0
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
