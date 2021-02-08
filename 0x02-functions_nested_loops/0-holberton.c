aef75d87c15ba077807493e19e33ee8b5315418e
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-08 23:48:50 +0000

    Created directory for 0x02-functions_nested_loops.

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - Prints holberton
+ *
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	_putschar('H');
+	_putschar('o');
+	_putschar('l');
+	_putschar('b');
+	_putschar('e');
+	_putschar('r');
+	_putschar('t');
+	_putschar('o');
+	_putschar('n');
+	_putschar('\n');
+	
+	return (0);
+}
