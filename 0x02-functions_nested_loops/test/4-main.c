6186ddc09ecf1efb01b4ddb668b16b67461474e3
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-10 03:45:20 +0000

    Corrected 4-is_alpha.c

--- 0x02-functions_nested_loops/test/4-main.c
+++ 0x02-functions_nested_loops/test/4-main.c
@@ -1,21 +1,22 @@
 #include "../holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+ *  * main - check the code for Holberton School students.
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
     int r;
 
-    r = _islower('H');
+    r = _isalpha('H');
     _putchar(r + '0');
-    r = _islower('o');
+    r = _isalpha('o');
     _putchar(r + '0');
-    r = _islower(108);
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
 }
-
