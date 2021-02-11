114c4d682efa9d875d897ef7fc36cae315aa84d0
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-11 20:55:57 +0000

    Add 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints squares
+ *
+ * @size: determines square size
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int height;
+	int width;
+
+	for (height = 0; height < size; height++)
+	{
+		for (width = 0; width < size; width++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
