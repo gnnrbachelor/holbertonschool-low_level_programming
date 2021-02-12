20a3243d22d903ae888431964c9caca4654d213a
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-12 16:35:33 +0000

    Fix 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,26 +13,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz");
-			if (i != 100)
-				printf(" ");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-			printf(" ");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-			printf(" ");
-		}
 		else
-		{
 			printf("%d", i);
+		if (i < 100)
 			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
