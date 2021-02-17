5d4f3a3d29d3c1b17a7c98175a35a0b2b9e113df
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-17 17:28:05 +0000

    Add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of string
+ *
+ * @str: String
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		if (j >= i / 2)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
