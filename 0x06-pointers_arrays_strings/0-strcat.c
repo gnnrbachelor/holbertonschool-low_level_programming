5e763bd46ba533828c92ce733b229c1a16c824d9
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-18 14:14:32 +0000

    Add 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,26 @@
 #include "holberton.h"
+
+/**
+ * _strcat - Concatenates src to dest
+ *
+ * @dest: String
+ * @src: String
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int c = 0;
+
+	while (dest[len] != '\0')
+		len++;
+
+	while (src[c] != '\0')
+		dest[len++] = src[c++];
+
+	dest[len++] = '\0';
+
+	return (dest);
+}
