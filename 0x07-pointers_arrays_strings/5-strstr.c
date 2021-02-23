7f5035027f07c445d6eb0443d1984589cfac8d85
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-23 00:03:46 +0000

    Pushing original version of 5-strstr.c.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,10 +19,14 @@ char *_strstr(char *haystack, char *needle)
 	{
 		j = 0;
 		if (haystack[i] == needle[j])
-		{
 			match = i;
-			return (haystack + match);
+		while (haystack[i] == needle[j])
+		{
+			i++;
+			j++;
 		}
+		if (needle[j] == '\0')
+			return (haystack + match);
 	i++;
 	}
 	return (0);
