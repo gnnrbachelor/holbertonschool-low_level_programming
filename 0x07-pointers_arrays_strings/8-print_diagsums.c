155fae0377e4595a7cdecfd26d444269b401b7a8
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-23 17:27:17 +0000

    Add 101-crackme_password

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - Prints diagonal sums
+ *
+ * @a: pointer to array
+ * @size: of array
+ *
+ * Return: Sum of values
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+}
