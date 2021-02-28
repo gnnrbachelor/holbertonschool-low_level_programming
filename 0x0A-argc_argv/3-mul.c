3cfbc063963a85249e769d0af100af19294ecad9
gnnrbachelor <gnnrbachelor@gmail.com>
2021-02-28 21:03:47 +0000

    Add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints arguments
+ *
+ * @argc: Number of arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y, z;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	z = x * y;
+
+	printf("%d\n", z);
+
+	return (0);
+}
+
