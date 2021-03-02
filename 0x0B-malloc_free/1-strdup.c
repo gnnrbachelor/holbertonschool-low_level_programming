45245f849dda205b1b52ef597368580a6cd5c63f
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-02 15:58:21 +0000

    Add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns pointer to copied string
+ *
+ * @str: String to copy
+ *
+ * Return: Pointer to string or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	int len = 0;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		len++;
+
+	array = malloc(sizeof(char) * len);
+
+	for (j = 0; str[j]; j++)
+		array[j] = str[j];
+
+	array[len] = '\0';
+
+	return (array);
+}
