68a2c6ab463ec0e635edbbfcbe950687e495c2d0
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-03 00:26:59 +0000

    Fix 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -35,10 +35,15 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 
+	}
+
+	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
 	}
+
 	return (grid);
 }
