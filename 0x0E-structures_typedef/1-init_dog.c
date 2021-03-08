83315b16d8196bc81ad8c5f352c55227f6492649
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-08 22:12:17 +0000

    Add 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes dog struct
+ *
+ * @d: struct
+ * @name: name
+ * @age: age
+ * @owner: owner name
+ *
+ * Return: Void;
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
