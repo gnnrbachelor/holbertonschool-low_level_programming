b55fea48aa05bfda319da6a94f256a77cbe09432
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-08 21:32:23 +0000

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Data structure for a dog
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner name
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
