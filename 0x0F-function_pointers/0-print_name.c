5a1d07ad7e0303f16a002df767a57128d576f6ab
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-10 20:09:50 +0000

    Add 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_name - Prints name
+ *
+ * @name: name
+ * @f: pointer to function
+ *
+ * Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
+
+
