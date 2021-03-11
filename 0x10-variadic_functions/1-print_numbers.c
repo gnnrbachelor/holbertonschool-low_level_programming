5a6655319c3079cfcd17d13c409485705efde20a
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-11 20:58:38 +0000

    Add 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_numbers - Prints numbers
+ *
+ * @separator: String to print between num
+ * @n: Number of ints
+ *
+ * Return: Void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list numargs;
+	unsigned int current_number;
+
+	if (n == 0)
+		return;
+
+	va_start(numargs, n);
+
+	for (i = 0; i < n; i++)
+	{
+		current_number = va_arg(numargs, int);
+		if (i != n - 1 && separator != NULL)
+			printf("%d%s", current_number, separator);
+		else
+			printf("%d", current_number);
+
+	}
+
+	va_end(numargs);
+
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
