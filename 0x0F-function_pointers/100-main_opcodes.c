4e4c92f0161a82ee3ec3aa6a822af60f39c4a7d6
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-12 19:42:20 +0000

    Fix 3-main.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - Prints opcodes
+ *
+ * @argc: count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+
+}
+
+
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,11 +12,17 @@
  * Return: Always 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	int a, b, answer;
 	char *c;
 	int (*calculate)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
