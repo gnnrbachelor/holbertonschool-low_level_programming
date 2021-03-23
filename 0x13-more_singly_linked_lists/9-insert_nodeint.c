67450e8b7a95996c320303736ad76983be1935a9
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-23 23:16:41 +0000

    Fix 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	for (i = 0; i < idx - 1; i++)
+	{
 		temp = temp->next;
+
+		if (temp == NULL)
+			return (NULL);
+	}
 
 	n_node->next = temp->next;
 	temp->next = n_node;
