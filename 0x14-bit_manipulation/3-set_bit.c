63c0b24cbcb6423384f94ce18230e665d066b608
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-26 01:37:42 +0000

    Fix 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,5 +11,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	return ((1 << index) | *n);
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n = *n | 1 << index;
+	return (1);
 }
