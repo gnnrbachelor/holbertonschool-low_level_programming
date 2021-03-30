ce5cb35176a501748b6897491677a9eee89de387
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-30 21:56:37 +0000

    Add 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * append_text - Appends text to end
+ *
+ * @filename: File name
+ * @text_content: Text Content
+ *
+ * Return: 1 or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, len, count;
+
+	if (!filename)
+		return (-1);
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	file = open(filename, O_APPEND | O_WRONLY);
+	if (file == -1)
+		return (-1);
+
+	if (len != 0)
+	{
+		count = write(file, text_content, len);
+		if (count == -1)
+			return (-1);
+	}
+
+	close(file);
+	return (1);
+}
