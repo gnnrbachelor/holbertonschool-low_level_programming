0a337ae658af24364adddfa954ab9de31d1c679c
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-30 21:03:40 +0000

    Add 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * create_file - Creates file
+ *
+ * @filename: File Name
+ * @text_content: String
+ *
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, len, count;
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	count = write(file, text_content, len);
+
+	if (count == -1)
+	{
+		write(1,"Fails", 5);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
