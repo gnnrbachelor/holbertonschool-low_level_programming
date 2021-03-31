bca3a2744a72b195e8b132293f3ebc411b8700d0
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-31 15:41:27 +0000

    Fix 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,19 +13,8 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, len, count = 0;
 
-	for (len = 0; text_content[len]; len++)
-		;
-
 	if (!filename)
 		return (-1);
-
-	if (!text_content)
-	{
-		file = open(filename, O_CREAT, 0600);
-		if (file == -1)
-			return (-1);
-		return (1);
-	}
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
@@ -33,14 +22,17 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+		;
+
 		count = write(file, text_content, len);
 
-	if (count == -1)
-	{
-		write(1, "Fails", 5);
+		if (count == -1)
 		return (-1);
 	}
 
 	close(file);
+
 	return (1);
 }
