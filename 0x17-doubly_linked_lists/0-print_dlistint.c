418b6cb5b92e0be21339a35d8aa34ff96a9741b4
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-14 23:18:24 +0000

    Add 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,23 @@
 #include "lists.h"
 
 /**
+ * print_dlistint - Prints all elements of linked list
  *
+ * @h: Head
  *
- *
- *
- *
+ * Return: # of nodes
  */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	int i;
+
+	for (i = 0; temp != NULL; i++)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	return i;
+}
 
