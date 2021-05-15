1a313bc459b7a1b58561420d3652b4549eff37e2
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-15 18:41:30 +0000

    Add 6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * dlistint_t - Sums nodes
+ *
+ * @head: Head
+ *
+ * Return: Sum of all nodes or 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+
+	dlistint_t *temp = head;
+	int sum = 0;
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+
+}
