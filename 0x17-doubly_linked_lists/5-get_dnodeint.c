0a81b853a98b2970f2af7f12035aeffafdaa4eca
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-15 17:47:56 +0000

    Add get_nodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Gets nth node
+ *
+ * @head: Head
+ *
+ * @index: Index
+ *
+ * Return: Node or NULL
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	if (index == 0)
+		return (NULL);
+
+	for (; temp != NULL && i < index; i++)
+		temp = temp->next;
+
+	return (temp);
+}
