a73e610784e2abbf1792db058697b3f5896f52fc
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-16 18:38:34 +0000

    Fix 8-delete_dnodeint

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 
 	temp->prev->next = temp->next;
-	temp->next->prev = temp->prev;
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
 	free(temp);
 	return (1);
 }
