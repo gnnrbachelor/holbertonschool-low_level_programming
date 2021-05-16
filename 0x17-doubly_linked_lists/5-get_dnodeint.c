afd7193af111f27a61d264bd8088306b402ad8dd
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-16 17:27:45 +0000

    Fix 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	if (index == 0)
-		return (NULL);
+		return (head);
 
 	for (; temp != NULL; i++)
 	{
