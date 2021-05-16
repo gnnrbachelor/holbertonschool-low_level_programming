9b61be62881d9740bfa3ba3141e82b608d41b2a5
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-16 18:20:42 +0000

    Fix 7-add_dnone

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,8 +20,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (; i != idx && temp->next != NULL ; i++)
+	for (; i < idx - 1 && temp != NULL ; i++)
+	{
 		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
 
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
