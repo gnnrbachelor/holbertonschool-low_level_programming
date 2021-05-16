e7d8778ab1282172ccd98bc001512a4309778a7d
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-16 17:50:12 +0000

    Fix 7-insert_dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,10 +23,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (; i < idx && temp != NULL; i++)
-	{
+	for (; i < idx && temp->next != NULL; i++)
 		temp = temp->next;
-	}
 
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
